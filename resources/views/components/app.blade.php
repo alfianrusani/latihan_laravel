61a760da79303c83628937bd5186db2a7edef626
Andi Alfian Rusani <alsanirufian@gmail.com>
2026-04-08 14:05:21 +0800

    Pertemuan 2: Blade Template

---
+++ resources/views/components/app.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>{{ $title }}</title>
+</head>
+
+<body>
+    {{-- Page title --}}
+    <div class="bg-primary py-5 text-center text-white">
+        <h1 class="fw-bold">{{ $title }}</h1>
+    </div>
+
+    {{-- Main app --}}
+    <div class="container my-5">
+        {{ $slot }}
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+
+
+</html>
